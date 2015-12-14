53e3f90691430d01e87f3c3fc97d3b0d555f37dd
Damien Duboeuf <smeagolworms4@gmail.com>
2015-12-14 17:36:24 +0100

    Update base plugin

---
+++ ZTPlugin.js
@@ -0,0 +1,33 @@
+GollumJS.Utils.global().ZTPlugin = new GollumJS.Class({
+	
+	Extends: App.Plugin,
+
+	initialize: function (metaInfos) {
+		console.debug ('Create instance '+metaInfos.id);
+	},
+
+	beforeLoad: function () {
+		console.debug ('ZTPlugin beforeLoad');
+	},
+
+	load: function () {
+		console.debug ('ZTPlugin load');
+	},
+
+	afterLoad: function () {
+		console.debug ('ZTPlugin afterLoad');
+	},
+
+	beforeUnload: function () {
+		console.debug ('ZTPlugin beforeUnload');
+	},
+
+	unLoad: function () {
+		console.debug ('ZTPlugin unLoad');
+	},
+
+	afterUnload: function () {
+		console.debug ('ZTPlugin afterUnload');
+	}
+
+});
